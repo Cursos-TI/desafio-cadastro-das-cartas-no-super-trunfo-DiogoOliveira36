c1b1b74b79aec02c94fd8445e9f523738ca236e5
DiogoOliveira36 <diogo.raphael36@gmail.com>
2025-02-19 02:32:00 +0000

    Finalizando Carta 1

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -18,5 +18,38 @@ int main() {
     // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
+    char estado1, estado2;
+    char codigo1[3], codigo2[3];
+    char cidade1[50], cidade2[50];
+    int populacao1, polulacao2;
+    float area1, area2;
+    float pib1, pib2;
+    int pontos_turisticos1, pontos_turisticos2;
+
+    // Carta 1
+    printf("Informe o estado da Carta 1 (A-H): \n");
+    scanf("%s", &estado1);
+    
+    printf("Informe o código da Carta 1 (ex. A01): \n");
+    scanf("%s", &codigo1);
+    
+    printf("Informe o nome da cidade da Carta 1: \n");
+    scanf("%s", &cidade1);
+    
+    printf("Informe a população da cidade da Carta 1: \n");
+    scanf("%d", &populacao1);
+
+    printf("Informe a área da cidade da Carta 1: \n");
+    scanf("%f", &area1);
+
+    printf("Informe o PIB da cidade da Carta 1: \n");
+    scanf("%f", &pib1); 
+
+    printf("Informe o Ponto Turístico da cidade da Carta 1: \n");
+    scanf("%d", &pontos_turisticos1);
+
+
+
+
     return 0;
 }
